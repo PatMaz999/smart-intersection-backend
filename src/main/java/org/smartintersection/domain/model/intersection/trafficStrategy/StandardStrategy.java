2a9167343b2ad795aff0ed24b0ddda36944ef576
PatMaz999 <Patryk.m.3456@gmail.com>
2026-02-24 00:11:34 +0100

    fix bug with adding invalid yellow stage

--- src/main/java/org/smartintersection/domain/model/intersection/trafficStrategy/StandardStrategy.java
+++ src/main/java/org/smartintersection/domain/model/intersection/trafficStrategy/StandardStrategy.java
@@ -1,7 +1,6 @@
 package org.smartintersection.domain.model.intersection.trafficStrategy;
 
 import org.smartintersection.domain.model.intersection.Direction;
-import org.smartintersection.domain.model.intersection.lanes.Lane;
 import org.smartintersection.domain.model.intersection.lanes.LanesGroup;
 import org.smartintersection.domain.model.intersection.lightsState.LightColor;
 import org.smartintersection.domain.model.intersection.lightsState.LightsState;
@@ -13,7 +12,6 @@ import org.smartintersection.domain.model.intersection.trafficStrategy.chainStra
 import org.smartintersection.domain.model.intersection.trafficStrategy.chainStrategy.EmptyGreenHandler;
 import org.smartintersection.domain.model.intersection.trafficStrategy.chainStrategy.EmptyRoadHandler;
 import org.smartintersection.domain.model.intersection.trafficStrategy.chainStrategy.PriorityHandler;
-import org.smartintersection.domain.model.vehicle.TurnDirection;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -55,46 +53,12 @@ public class StandardStrategy implements TrafficStrategy {
     }
 
     private ScheduledState findClearancePhase(LightsState currentLightState, LightsState newLightState) {
-        if (newLightState instanceof SingleLaneGreen && currentLightState instanceof StraightLineGreen && currentLightState.getLightColors().containsKey(newLightState.getByColor(LightColor.GREEN).iterator().next())) {
+        if (newLightState instanceof SingleLaneGreen && currentLightState instanceof StraightLineGreen) {
             Direction newGreenDirection = newLightState.getByColor(LightColor.GREEN).iterator().next();
-            return new ScheduledState(new ClearSingleLane(currentLightState, newGreenDirection.getOpposite()), 1);
+
+            if (currentLightState.getLightColors().get(newGreenDirection) == LightColor.GREEN)
+                return new ScheduledState(new ClearSingleLane(currentLightState, newGreenDirection.getOpposite()), 1);
         }
         return new ScheduledState(new ClearancePhase(newLightState), 1);
-    }
-
-
-//    private void swapToOneLine(LightsState currentState, Direction priorityDirection, int duration) {
-//        statesQueue.add(new ScheduledState(new ClearSingleLane(currentState, priorityDirection.getOpposite()), 1));
-//        statesQueue.add(new ScheduledState(new SingleLaneGreen(priorityDirection), duration));
-//    }
-//
-//    private void wrapWithClearancePhase(LightsState currentState, LightsState newState, int duration) {
-//        statesQueue.add(new ScheduledState(new ClearancePhase(currentState), 1));
-//        statesQueue.add(new ScheduledState(newState, duration));
-//    }
-//
-//    private void extendCurrentPhase(LightsState currentState, int duration) {
-//        statesQueue.add(new ScheduledState(currentState, duration));
-//    }
-
-    //    TODO: to remove
-    private boolean requiresDedicatedLeftTurn(LanesGroup lanes) {
-        for (Lane lane : lanes.getLanes().values()) {
-            var nextTurn = lane.nextCarTurnDirection();
-            if (nextTurn.isPresent() && nextTurn.get() != TurnDirection.LEFT)
-                continue;
-            int steps = 0;
-            int series = 0;
-            for (var qu : lane.getQueue()) {
-                if (qu.getTurnDirection() == TurnDirection.LEFT)
-                    series++;
-                steps++;
-                if (steps >= 5)
-                    break;
-            }
-            if (series >= 3)
-                return true;
-        }
-        return false;
     }
 }
--- src/test/java/org/smartintersection/domain/model/intersection/trafficStrategy/StandardStrategyTest.java
+++ src/test/java/org/smartintersection/domain/model/intersection/trafficStrategy/StandardStrategyTest.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.smartintersection.domain.model.intersection.Direction;
 import org.smartintersection.domain.model.intersection.lanes.LanesGroup;
-import org.smartintersection.domain.model.intersection.lightsState.AbstractLightsState;
 import org.smartintersection.domain.model.intersection.lightsState.LightsState;
+import org.smartintersection.domain.model.intersection.lightsState.singleRoad.ClearSingleLane;
+import org.smartintersection.domain.model.intersection.lightsState.singleRoad.ClearancePhase;
+import org.smartintersection.domain.model.intersection.lightsState.singleRoad.SingleLaneGreen;
+import org.smartintersection.domain.model.intersection.lightsState.singleRoad.StraightLineGreen;
 import org.smartintersection.domain.model.intersection.trafficStrategy.chainStrategy.AbstractTrafficHandler;
-import org.smartintersection.domain.model.intersection.trafficStrategy.chainStrategy.EmptyRoadHandler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +47,248 @@ class StandardStrategyTest {
 //        then
         assertEquals(currentState, greenState);
         verify(trafficHandler, times(2)).handle(lanesGroup, currentState);
+    }
+
+    @Test
+    void shouldNotCalculateNewLightsStateWhenQueueIsNotEmpty(){
+//        given
+        ScheduledState scheduledState = new ScheduledState(currentState, 3);
+
+        when(trafficHandler.handle(lanesGroup, currentState)).thenReturn(scheduledState);
+
+//        when
+        standardStrategy.changeLightsState(lanesGroup, currentState);
+        standardStrategy.changeLightsState(lanesGroup, currentState);
+        standardStrategy.changeLightsState(lanesGroup, currentState);
+
+//        then
+        verify(trafficHandler, times(1)).handle(lanesGroup, currentState);
+    }
+
+
+    @Test
+    void shouldWrapWithFullClearanceWhenChangingStraightLineToOppositeNorth() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.NORTH);
+        LightsState newState = new StraightLineGreen(Direction.EAST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingStraightLineToOppositeWest() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.EAST);
+        LightsState newState = new StraightLineGreen(Direction.NORTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingFromSingleLaneForNorth() {
+        // given
+        LightsState currentState = new SingleLaneGreen(Direction.NORTH);
+        LightsState newState = new StraightLineGreen(Direction.NORTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingFromSingleLaneForSouth() {
+        // given
+        LightsState currentState = new SingleLaneGreen(Direction.NORTH);
+        LightsState newState = new StraightLineGreen(Direction.SOUTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingFromSingleLaneForWest() {
+        // given
+        LightsState currentState = new SingleLaneGreen(Direction.WEST);
+        LightsState newState = new StraightLineGreen(Direction.WEST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingFromSingleLaneForEast() {
+        // given
+        LightsState currentState = new SingleLaneGreen(Direction.WEST);
+        LightsState newState = new StraightLineGreen(Direction.EAST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+
+    @Test
+    void shouldWrapWithPartialClearance_WhenChangingStraightToSingleOnSameAxisNorth() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.NORTH);
+        LightsState newState = new SingleLaneGreen(Direction.NORTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearSingleLane.class, result);
+    }
+
+    @Test
+    void shouldWrapWithPartialClearance_WhenChangingStraightToSingleOnSameAxisSouth() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.NORTH);
+        LightsState newState = new SingleLaneGreen(Direction.SOUTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearSingleLane.class, result);
+    }
+
+    @Test
+    void shouldWrapWithPartialClearance_WhenChangingStraightToSingleOnSameAxisWest() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.WEST);
+        LightsState newState = new SingleLaneGreen(Direction.WEST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearSingleLane.class, result);
+    }
+
+    @Test
+    void shouldWrapWithPartialClearance_WhenChangingStraightToSingleOnSameAxisEast() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.WEST);
+        LightsState newState = new SingleLaneGreen(Direction.EAST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearSingleLane.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingStraightToSingleOnDifferentAxisEast() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.NORTH);
+        LightsState newState = new SingleLaneGreen(Direction.EAST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingStraightToSingleOnDifferentAxisWest() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.NORTH);
+        LightsState newState = new SingleLaneGreen(Direction.WEST);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingStraightToSingleOnDifferentAxisNorth() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.EAST);
+        LightsState newState = new SingleLaneGreen(Direction.NORTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
+    }
+
+    @Test
+    void shouldWrapWithFullClearance_WhenChangingStraightToSingleOnDifferentAxisSouth() {
+        // given
+        LightsState currentState = new StraightLineGreen(Direction.EAST);
+        LightsState newState = new SingleLaneGreen(Direction.SOUTH);
+
+        when(trafficHandler.handle(lanesGroup, currentState))
+                .thenReturn(new ScheduledState(newState, 5));
+
+        // when
+        LightsState result = standardStrategy.changeLightsState(lanesGroup, currentState);
+
+        // then
+        assertInstanceOf(ClearancePhase.class, result);
     }
 
 }
